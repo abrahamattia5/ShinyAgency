d11a6b75566e863b7fe88147bff1575e00a7c8e6
Abraham <abraham.attia.pro@gmail.com>
2024-06-05 13:43:20 +0200

    mise en place de la navigation entre les pages grace aux liens : creation d'un header avec les liens :  /components/Header/index.jsx qui determine les liens possible en fonction des routes
     ajout de mon Header dans l'index principal , a l'interieur du router src/index.tsx afin de l'afficher et permettre la navigation (en fonction des routes determiné precedement)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import Home from './pages/Home';
 import Survey from './pages/Survey';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<Survey />} />
