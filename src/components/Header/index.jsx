be0cf1ece6cdb1ef19875a2d48740b5ff0114fb9
Abraham <abraham.attia.pro@gmail.com>
2024-06-09 11:12:46 +0200

    styliser un element d'une bibliotheque (ici link)(et non du dom)
    ajout du style aux liens du composant Header grace a styled-components dans  Header/index.jsx
    
    syntaxe : au lieu de styled.span`css' -> styled(Link)`css'

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)
+`
+    padding: 15px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+`
  
 function Header() 
 {
     return (
         <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/survey/1">Questionnaire</Link>
-            <Link to="/freelances">Freelances</Link>
+            <StyledLink to="/">       Accueil</StyledLink>
+            <StyledLink to="/survey/1">Questionnaire</StyledLink>
+            <StyledLink to="/freelances">Profils</StyledLink>
         </nav>
     )
 }
