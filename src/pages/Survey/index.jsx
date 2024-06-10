6ed619e4d447153ead2fa018a70e56bc673a932c
Abraham <abraham.attia.pro@gmail.com>
2024-06-10 11:50:08 +0200

    creation du style de la page d'enquette Survey grace aux styled-components

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,32 +1,65 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
 
-function Survey() 
+
+const SurveyContainer = styled.div
+`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2
+`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span
+`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div
+`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
+
+function Survey()
 {
+    
     const { questionNumber } = useParams()
-    // const [params] = useSearchParams()
-    // console.log(params.get('category'))
 
     const questionNumberInt = parseInt(questionNumber)
     const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
     const nextQuestionNumber = questionNumberInt + 1
 
+  
+
     return (
-        <div>
-            <h1>Questionnaire 🧮</h1>
-            <h2>Question {questionNumber}</h2>
+        <SurveyContainer>
+            <QuestionTitle>Question {questionNumber}</QuestionTitle>
 
-            <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+            <QuestionContent>contrenue de la question</QuestionContent>
 
-            {questionNumberInt === 10 ? 
-            (
-                <Link to="/results">Résultats</Link>
-            ) : 
-            (
+            <LinkWrapper>
+                <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+                {[questionNumberInt + 1] ? (
                 <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-            )}
-
-        </div>
+                ) : (
+                <Link to="/results">Résultats</Link>
+                )}
+            </LinkWrapper>
+        </SurveyContainer>
     )
 }
 
