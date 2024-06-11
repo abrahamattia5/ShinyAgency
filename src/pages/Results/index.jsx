809ecea8afda9b6608a8fb1be1cc274ef137dece
Abraham <abraham.attia.pro@gmail.com>
2024-06-11 19:28:55 +0200

    recuperation des resultats de pages/Survey dans pages/Results grace au context SurveyContext  qui contiens l'objet answers qui contiens les reponses.
    Les reponses sont pour le moment afficher sur la page et dasn la console dans la pages/Results
    
    => contiens une erreur : si on ne rempli pas oui ou non la reponse est ignorée

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -1,10 +1,36 @@
+import { useContext } from 'react'
+import { SurveyContext } from '../../utils/context'
+
 function Results() 
 {
+  const { answers } = useContext(SurveyContext)
+  console.log(answers)
+
     return (
       <div>
         <h1>Résultats</h1>
+        <p>Voici les réponses que vous avez données :</p>
+        <ul>
+          {Object.keys(answers).map((questionNumber) => (
+            <li key={questionNumber}>
+              Question {questionNumber} : {answers[questionNumber] ? 'Oui' : 'Non'}
+            </li>
+          ))}
+        </ul>
+
       </div>
     )
 }
-  
   export default Results
+
+/*
+//Object.keys permet de récupérer les clés d'un objet ici les numéros de questions de l'enquête Survey sous forme de tableau
+<p>Voici les réponses que vous avez données :</p>
+<ul>
+  {Object.keys(answers).map((questionNumber) => (
+    <li key={questionNumber}>
+      Question {questionNumber} : {answers[questionNumber] ? 'Oui' : 'Non'}
+    </li>
+  ))}
+</ul>
+*/
