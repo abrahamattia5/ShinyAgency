efb2e045ab955dcb0600ac0307192f922b88efcc
Abraham <abraham.attia.pro@gmail.com>
2024-06-11 15:20:07 +0200

    creation d'un contexte pour garder les resultats de l'enquette 'survey' sauvegardées en JSON dans la variable 'answers' du state du context . 'saveAnswers' permettera de changer l'etat de 'answers'
    dans utils/context/index.jsx  creation du context de l'enquette "SurveyContext" qui permettra de sauvegarder les réponses de l'enquette Survey dans le contexte et ainsi de proposer un resultat

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -21,3 +21,27 @@ export const ThemeProvider = ({ children }) =>
         </ThemeContext.Provider>
     )
 }
+
+// ---------------- 
+
+//creation du contexte qui contiendra les réponses de l'enquête Survey
+export const SurveyContext = createContext()
+
+export const SurveyProvider = ({ children }) => 
+{
+    //état initial de la variable answers
+    const [answers, setAnswers] = useState({})
+
+    //fonction pour sauvegarder les réponses. elle sont sauvegardées en JSON dans la variable answers
+    const saveAnswers = (newAnswers) => 
+    {
+        setAnswers({ ...answers, ...newAnswers })
+    }
+
+    //fourniture du contexte grace à la méthode Provider de SurveyContext avec les valeurs answers et saveAnswers qui permettront de sauvegarder les réponses de l'enquette Survey dans le contexte
+    return (
+        <SurveyContext.Provider value={{ answers, saveAnswers }}>
+        {children}
+        </SurveyContext.Provider>
+    )
+}
