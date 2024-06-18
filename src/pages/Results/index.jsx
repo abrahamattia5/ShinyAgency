8d52c51cfa7ed405659feb879af6bf522fbae013
Abraham <abraham.attia.pro@gmail.com>
2024-06-18 17:34:54 +0200

    creation de la page result a partir des 2 hooks customs : useState et useTheme
    utilosation du SurveyContext pour recuperer et traiter les resultats de l'enquette et afficher les données correspondantes

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -1,36 +1,128 @@
 import { useContext } from 'react'
 import { SurveyContext } from '../../utils/context'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import { useFetch, useTheme } from '../../utils/hooks'
+import { StyledLink, Loader } from '../../utils/style/Atoms'
+
+const ResultsContainer = styled.div
+`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 60px 90px;
+  padding: 30px;
+  background-color: ${({ theme }) => theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+`
+
+const ResultsTitle = styled.h2
+`
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  font-weight: bold;
+  font-size: 28px;
+  max-width: 60%;
+  text-align: center;
+  & > span {
+    padding-left: 10px;
+  }
+`
+
+const DescriptionWrapper = styled.div
+`
+  padding: 60px;
+`
+
+const JobTitle = styled.span
+`
+  color: ${({ theme }) => theme === 'light' ? colors.primary : colors.backgroundLight};
+  text-transform: capitalize;
+`
+
+const JobDescription = styled.div
+`
+  font-size: 18px;
+  & > p {
+    color: ${({ theme }) => (theme === 'light' ? colors.secondary : '#ffffff')};
+    margin-block-start: 5px;
+  }
+  & > span {font-size: 20px;}
+`
+
+const LoaderWrapper = styled.div
+`
+  display: flex;
+  justify-content: center;
+`
+
+function formatFetchParams(answers) 
+{
+  const answerNumbers = Object.keys(answers)
+
+  return answerNumbers.reduce((previousParams, answerNumber, index) => 
+  {
+    const isFirstParam = index === 0
+    const separator = isFirstParam ? '' : '&'
+    return `${previousParams}${separator}a${answerNumber}=${answers[answerNumber]}`
+  }, '')
+}
 
 function Results() 
 {
+  const { theme } = useTheme()
   const { answers } = useContext(SurveyContext)
-  console.log(answers)
+  const fetchParams = formatFetchParams(answers)
 
-    return (
-      <div>
-        <h1>Résultats</h1>
-        <p>Voici les réponses que vous avez données :</p>
-        <ul>
-          {Object.keys(answers).map((questionNumber) => (
-            <li key={questionNumber}>
-              Question {questionNumber} : {answers[questionNumber] ? 'Oui' : 'Non'}
-            </li>
+  const { data, isLoading, error } = useFetch(`http://localhost:8000/results?${fetchParams}`)
+
+  if (error) 
+  {
+    return <span>Il y a un problème</span>
+  }
+  // "?" permet de verifier que data n'est pas null ou undefined avant d'essayer d'accéder à la propriété resultsData. Si data est null ou undefined, l'expression entière renverra undefined au lieu de provoquer une erreur. 
+  const resultsData = data?.resultsData
+
+  return isLoading ? 
+  (
+    <LoaderWrapper>
+      <Loader />
+    </LoaderWrapper>
+  ) : 
+  (
+    <ResultsContainer theme={theme}>
+      <ResultsTitle theme={theme}>
+        Les compétences dont vous avez besoin :
+        {
+        resultsData &&
+          resultsData.map((result, index) => 
+          (
+            <JobTitle
+              key={`result-title-${index}-${result.title}`}
+              theme={theme}
+            >
+              {result.title}
+              {index === resultsData.length - 1 ? '' : ','}
+            </JobTitle>
           ))}
-        </ul>
+      </ResultsTitle>
 
-      </div>
-    )
+      <StyledLink $isFullLink to="/freelances">
+        Découvrez nos profils
+      </StyledLink>
+
+      <DescriptionWrapper>
+        {resultsData &&
+          resultsData.map((result, index) => (
+            <JobDescription
+              theme={theme}
+              key={`result-detail-${index}-${result.title}`}
+            >
+              <JobTitle theme={theme}>{result.title}</JobTitle>
+              <p>{result.description}</p>
+            </JobDescription>
+          ))}
+      </DescriptionWrapper>
+    </ResultsContainer>
+  )
 }
-  export default Results
 
-/*
-//Object.keys permet de récupérer les clés d'un objet ici les numéros de questions de l'enquête Survey sous forme de tableau
-<p>Voici les réponses que vous avez données :</p>
-<ul>
-  {Object.keys(answers).map((questionNumber) => (
-    <li key={questionNumber}>
-      Question {questionNumber} : {answers[questionNumber] ? 'Oui' : 'Non'}
-    </li>
-  ))}
-</ul>
-*/
+export default Results
