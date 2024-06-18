906f3b889dc45bddf8189fb8fe70497d4d4b9ccd
Abraham <abraham.attia.pro@gmail.com>
2024-06-18 18:25:16 +0200

    dans la pages/Home
    gestion du theme
    
    utilisation du hook useTheme  qui récupère le Contexte du theme permet de récupérer le thème actuel (dark  ou light).

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { StyledLink } from '../../utils/style/Atoms'
 import HomeIllustration from '../../assets/home-illustration.svg'
+import { useTheme } from '../../utils/hooks'
 
 const HomeWrapper = styled.div`
   display: flex;
@@ -10,7 +11,7 @@ const HomeWrapper = styled.div`
 
 const HomerContainer = styled.div`
   margin: 30px;
-  background-color: ${colors.background};
+  background-color:  ${({ theme }) => theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
   padding: 60px 90px;
   display: flex;
   flex-direction: row;
@@ -31,18 +32,22 @@ const StyledTitle = styled.h2`
   padding-bottom: 30px;
   max-width: 280px;
   line-height: 50px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const Illustration = styled.img`
   flex: 1;
 `
 
-function Home() {
+function Home()
+{
+  const { theme } = useTheme()
+
   return (
     <HomeWrapper>
-      <HomerContainer>
+      <HomerContainer theme={theme} >
         <LeftCol>
-          <StyledTitle>
+          <StyledTitle theme={theme} >
             Repérez vos besoins, on s’occupe du reste, avec les meilleurs
             talents
           </StyledTitle>
