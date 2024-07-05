47162f1ebe64e4aa25af5459f2674522cef3b6da
Abraham <abraham.attia.pro@gmail.com>
2024-07-05 11:57:22 +0200

    test que le texte du bouton du  footer : 1- contiens bien son nom de depart 2- change bien son nom au clic
    Cela test aussi le hooks useTheme car pour tester un hook, le meilleur moyen est de tester un composant qui utilise ce hook. ici notre footer
    Fixes #8

--- src/components/Footer/index.test.js
+++ src/components/Footer/index.test.js
@@ -1,7 +1,7 @@
 // ---------> Test du composant footer qui pemet de changer de theme
 
 import Footer from './'
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 //le footer a besoin du contexte pour fonctionner, sans lui il crash
 import { ThemeProvider } from '../../utils/context'
 
@@ -17,4 +17,21 @@ describe('Footer', () =>
         )
     })
 
+    //verifie que le bouton de changement de theme fonctionne
+    test('should change theme', async () => 
+    {
+        render(
+            <ThemeProvider>
+                <Footer />
+            </ThemeProvider>
+        )
+        //screeen est un objet qui permet de récupérer des éléments du DOM
+        //getByRole permet de récupérer un élément par son role, ici un bouton (autre roles possible : link, heading, img, option, etc.)
+        const nightModeButton = screen.getByRole('button')
+        expect(nightModeButton.textContent).toBe('Changer de mode :  ☀️')
+        
+        //fireEvent permet de simuler un événement du DOM, ici un clic
+        fireEvent.click(nightModeButton)
+        expect(nightModeButton.textContent).toBe('Changer de mode :  🌙')
+    })
 })
