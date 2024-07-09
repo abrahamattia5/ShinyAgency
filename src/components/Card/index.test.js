d208c56f25a857f441867cd54b7e639cd2a6ee66
Abraham <abraham.attia.pro@gmail.com>
2024-07-09 13:46:15 +0200

    test du composant Card : tester que l'étoile de favori passe de ☆  à  ⭐️ au click
    Fixes #14

--- src/components/Card/index.test.js
+++ src/components/Card/index.test.js
@@ -21,4 +21,30 @@ describe("Card", () =>
         expect(cardPicture.src).toBe("http://localhost/myPicture.png");
         expect(cardTitle.textContent).toContain("Harry Potter☆");
     });
+
+    test("Should the ☆ change to ⭐️ on click", async () => 
+    {
+        render(
+            <ThemeProvider>
+                <Card
+                    title="Harry Potter"
+                    label="Magicien frontend"
+                    picture="/myPicture.png"
+                />
+            </ThemeProvider>
+        );
+
+        // Récupérer le titre de la carte
+        const cardTitle = screen.getByText(/Harry Potter/);
+
+        // Vérifier que le titre contient bien Harry Potter ☆
+        expect(cardTitle.textContent).toContain("Harry Potter☆");
+
+        // Cliquer sur l'étoile contenue dans un span dans le titre de la carte
+        const starElement = screen.getByText("☆");
+        fireEvent.click(starElement);
+
+        // Vérifier que le titre contient bien Harry Potter ⭐️
+        expect(cardTitle.textContent).toContain("Harry Potter⭐️");
+    });
 });
