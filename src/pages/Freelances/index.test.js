9f10fa84271d6f19d76cb3dc6c1674cda528238d
abraham <abraham.attia.pro@gmail.com>
2024-07-12 13:05:34 +0200

    test que la pages Freelances affiche bien les données simulées qu'on lui envoi
    -> teste invalide a ajuster
    
    Fixes #20

--- src/pages/Freelances/index.test.js
+++ src/pages/Freelances/index.test.js
@@ -11,6 +11,9 @@ Les données simulées sont appelées "mocks" = simulacres
 import { rest } from 'msw'
 import '@testing-library/jest-dom/extend-expect'
 import { setupServer } from 'msw/node'
+import { waitForElementToBeRemoved, screen } from '@testing-library/react'
+import { render } from '../../utils/test'
+import Freelances from './'
 
 const freelancersMockedData = [
     {
@@ -42,3 +45,15 @@ afterEach(() => server.resetHandlers())
 // Ferme la simulation d'API une fois que les tests sont finis
 afterAll(() => server.close())
 
+it('Should display freelancers names after loader is removed', async () => 
+{
+  render(<Freelances />)
+
+  // On vérifie que le loader est présent avant que les données soient chargées et qu'il disparait une fois les données chargées
+  await waitForElementToBeRemoved(() => screen.getByTestId('loader'))
+
+  expect(screen.getByText('Harry Potter')).toBeInTheDocument()
+  expect(screen.getByText('Hermione Granger')).toBeInTheDocument()
+  // On vérifie que le loader n'est plus présent
+  expect(screen.queryByTestId('loader')).not.toBeInTheDocument()
+})
