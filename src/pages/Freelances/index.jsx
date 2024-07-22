03fe2498bededd34c241d8226862087120d6e2aa
abraham <abraham.attia.pro@gmail.com>
2024-07-22 22:05:56 +0200

    ajout de la capaciter de cliquer sur un profile de la liste et de rediriger vers la fiche du profile cliqueé
    Fixes #23

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
 import { useFetch, useTheme } from '../../utils/hooks'
+import { Link } from 'react-router-dom'
 
 
 const CardsContainer = styled.div`
@@ -69,12 +70,14 @@ function Freelances()
         <CardsContainer>
           {freelancersList.map((profile, index) => 
           (
-            <Card
-              key={`${profile.name}-${index}`}
-              label={profile.job}
-              title={profile.name}
-              picture={profile.picture}
-            />
+            <Link key={`freelance-${profile.id}`} to={`/profile/${profile.id}`}>
+              <Card
+                key={`${profile.name}-${index}`}
+                label={profile.job}
+                title={profile.name}
+                picture={profile.picture}
+              />
+            </Link>
           ))}
         </CardsContainer>
       )}
