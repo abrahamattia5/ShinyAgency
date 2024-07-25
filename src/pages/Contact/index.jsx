9cd6518ad45b927ef8d0b68e6420c728e31308f4
abraham <abraham.attia.pro@gmail.com>
2024-07-26 00:19:45 +0200

    création du formulaire de contacte dans la page Contact
    Fixes #47

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -17,6 +17,46 @@ const Title = styled.h1`
   color: ${({ theme }) => (theme === 'light' ? colors.primary : 'white')};
 `;
 
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 70%;
+  margin: 0 auto;
+  border-radius: 15px;
+  gap: 15px;
+  margin-bottom: 30px;
+  background-color: ${({ theme }) => (theme === 'light' ? '#F1F1F1' : colors.dark)};
+  padding: 20px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  width: 100%;
+  max-width: 400px;
+  border: 1px solid ${({ theme }) => (theme === 'light' ? colors.dark : 'white')};
+  background-color: ${({ theme }) => (theme === 'light' ? colors.backgroundLight : colors.backgroundDark)};
+  color: ${({ theme }) => (theme === 'light' ? colors.dark : 'white')};
+`;
+
+const TextArea = styled.textarea`
+  padding: 10px;
+  width: 100%;
+  max-width: 400px;
+  height: 100px;
+  border: 1px solid ${({ theme }) => (theme === 'light' ? colors.dark : 'white')};
+  background-color: ${({ theme }) => (theme === 'light' ? colors.backgroundLight : colors.backgroundDark)};
+  color: ${({ theme }) => (theme === 'light' ? colors.dark : 'white')};
+`;
+
+const SubmitButton = styled.button`
+  padding: 10px 20px;
+  background-color: ${colors.primary};
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
 const Section = styled.section`
   margin-bottom: 20px;
 `;
@@ -46,12 +86,28 @@ const SocialIcon = styled.a`
   }
 `;
 
-function Contact() {
+function Contact() 
+{
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Future POST request to your API
+        console.log('Form data:');
+        };
+
+
   return (
     <ThemeContext.Consumer>
       {({ theme }) => (
         <ContactContainer theme={theme}>
           <Title theme={theme}>Contactez-nous</Title>
+
+          <FormContainer theme={theme} onSubmit={handleSubmit}>
+            <Input theme={theme} type="text" placeholder="Nom" required />
+            <Input theme={theme} type="tel" placeholder="Téléphone" required />
+            <Input theme={theme} type="email" placeholder="Email" required />
+            <TextArea theme={theme} placeholder="Votre message" required />
+            <SubmitButton type="submit">Envoyer</SubmitButton>
+          </FormContainer>
 
           <Section>
             <SectionTitle theme={theme}>Adresse</SectionTitle>
